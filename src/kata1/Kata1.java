c61e36791d83e9c77b9496597e392b91af4dcf1a
Darwin <Darwin@Darwin-EHM-PC>
2013-12-02 20:33:58 +0000

    Primer intento

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -6,7 +6,7 @@ public class Kata1 {
 
     public static void main(String[] args) {
         Student student = new Student("Darwin", new Date(93, 2, 4));
-        System.out.println(student.name());
-        System.out.println(student.getAge());
+        System.out.println("El nombre del estudiante es: " + student.name());
+        System.out.println("La edad del estudiante es: " + student.getAge() + " años");
     }
 }
